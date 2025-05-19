db43a4c1977feafb2bcb6b16ec63c7fc48071a49
xrb-script <truexrb@gmail.com>
2025-05-19 11:44:42 +0200

    Update al.lua

--- locales/al.lua
+++ locales/al.lua
@@ -1,1 +1,16 @@
+-- locales/al.lua
+-- This file is less critical now as messages are directly pulled from Config.Messages.
+-- You would only need this if you were adapting the script to use QBCore's Lang() functions
+-- or adding language keys not present in the Config.
 
+-- Example if you were using Lang():
+-- Lang = Lang or {}
+-- Lang['al'] = Lang['al'] or {}
+
+-- Lang['al']['some_new_message_key'] = "Ky eshte nje mesazh i ri."
+
+-- You could still copy Config.Messages here for reference if desired, but it's not directly used by the script as converted.
+
+-- xResul Albania
+
+-- Discord : http://discord.gg/CAyUh9su2s
